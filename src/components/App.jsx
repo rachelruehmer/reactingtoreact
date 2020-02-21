10c39074aff13a9a7d2c17144667dfa81a3bbcab
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-21 12:12:40 -0600

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,41 @@ import React from "react";
 
 // function App(props) {
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: 'here we go'};
-  }  
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "here we go",
+      loaded: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    this.setState({
+      loaded: true
+    });
+  }
+
   render() {
-    return (
-<React.Fragment>
-<input
- value={this.state.text}
- onChange={(event) => { this.setState({ text: event.target.value })}} />
-
-
-    <h1>Hello, my name is {this.props.name} and {this.state.text}.</h1>
-     </React.Fragment>
-     );
-}
-
+    if (this.state.loaded) {
+      return <h1>Welcome!</h1>;
+    } else {
+      return (
+        <React.Fragment>
+          <h3>Loading...</h3>
+          <button onClick={this.handleClick}>Click Me!</button>
+          <input
+            value={this.state.text}
+            onChange={event => {
+              this.setState({ text: event.target.value });
+            }}
+          />
+          <h1>
+            Hello, my name is {this.props.name} and {this.state.text}.
+          </h1>
+        </React.Fragment>
+      );
     }
-
-
+  }
+}
 
 export default App;
